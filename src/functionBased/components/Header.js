c45ec8e988060366e97f1cac7ee842a587b8ce74
sameh <sameh080081@gmail.com>
2022-08-16 13:58:16 +0400

    fix linters

--- src/functionBased/components/Header.js
+++ src/functionBased/components/Header.js
@@ -10,10 +10,8 @@ const Header = () => {
     <header style={headerStyle}>
       <h1 style={
         {
-          fontSize: '6rem', fontWeight: '600', marginBottom: '2rem', lineHeight: '1em', color: '#ececec', textTransform: 'lowercase', textAlign: 'center'
-        }
-        }>
-          todos</h1>
+          fontSize: '6rem', fontWeight: '600', marginBottom: '2rem', lineHeight: '1em', color: '#ececec', textTransform: 'lowercase', textAlign: 'center',
+        }}>todos</h1>
     </header>
   );
 };
