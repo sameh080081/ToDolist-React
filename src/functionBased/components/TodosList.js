0c9dd1bd2f499df32b47b58edbd419cb034fdeae
sameh <sameh080081@gmail.com>
2022-08-16 19:18:24 +0400

    fix linters

--- src/functionBased/components/TodosList.js
+++ src/functionBased/components/TodosList.js
@@ -3,17 +3,17 @@ import TodoItem from './TodoItem';
 /* eslint-disable react/prop-types */
 /* eslint-disable react/destructuring-assignment */
 const TodosList = (props) => (
-    <ul>
-      {props.todos.map((todo) => (
-        <TodoItem
-          key={todo.id}
-          todo={todo}
-          handleChangeProps={props.handleChangeProps}
-          deleteTodoProps={props.deleteTodoProps}
-          setUpdate={props.setUpdate}
-        />
-      ))}
-    </ul>
-  );
-  
+  <ul>
+    {props.todos.map((todo) => (
+      <TodoItem
+        key={todo.id}
+        todo={todo}
+        handleChangeProps={props.handleChangeProps}
+        deleteTodoProps={props.deleteTodoProps}
+        setUpdate={props.setUpdate}
+      />
+    ))}
+  </ul>
+);
+
 export default TodosList;
