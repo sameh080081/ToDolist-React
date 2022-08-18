84bf34046efbab988d8eb21b569a821cf5e8cccc
sameh <sameh080081@gmail.com>
2022-08-18 15:09:08 +0400

    Updates

---
+++ precache-manifest.096d36101e12617eca4e4e93e6ec8412.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "13bcf8cc21fd00b26688",
+    "url": "/ToDoList-React/build/static/css/main.3da9992f.chunk.css"
+  },
+  {
+    "revision": "13bcf8cc21fd00b26688",
+    "url": "/ToDoList-React/build/static/js/main.13bcf8cc.chunk.js"
+  },
+  {
+    "revision": "5eda56d7abbc6c9043b9",
+    "url": "/ToDoList-React/build/static/js/1.5eda56d7.chunk.js"
+  },
+  {
+    "revision": "0ef1bbc54f6427ad5d19",
+    "url": "/ToDoList-React/build/static/js/runtime~main.0ef1bbc5.js"
+  },
+  {
+    "revision": "0dc058d22b7069c73c4bda5dc329d667",
+    "url": "/ToDoList-React/build/index.html"
+  }
+];
