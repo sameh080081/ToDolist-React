3f501f61545e030925cdf2df5cdce036e68e14c8
sameh <sameh080081@gmail.com>
2022-08-19 00:10:59 +0400

    commit deploy

---
+++ build/precache-manifest.26bc90d4d0045e1a03a0e9db656d507a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "eface17f5c7cae90d3ff",
+    "url": "/ToDoList-React/static/css/main.3da9992f.chunk.css"
+  },
+  {
+    "revision": "eface17f5c7cae90d3ff",
+    "url": "/ToDoList-React/static/js/main.eface17f.chunk.js"
+  },
+  {
+    "revision": "5eda56d7abbc6c9043b9",
+    "url": "/ToDoList-React/static/js/1.5eda56d7.chunk.js"
+  },
+  {
+    "revision": "c9cd1d89243e293abd6c",
+    "url": "/ToDoList-React/static/js/runtime~main.c9cd1d89.js"
+  },
+  {
+    "revision": "56c428b80c23d5f956d8a72a6cde1d3e",
+    "url": "/ToDoList-React/index.html"
+  }
+];
